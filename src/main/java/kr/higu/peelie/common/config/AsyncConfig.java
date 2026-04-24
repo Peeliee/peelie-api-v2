07d1060875b0acc65ed56f708814624fd93f0d5c
heegu123 <kookminkhg@kookmin.ac.kr>
2026-04-24 18:50:07 +0900

    feat: 비동기 처리를 위한 설정 클래스 추가

---
+++ src/main/java/kr/higu/peelie/common/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package kr.higu.peelie.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
