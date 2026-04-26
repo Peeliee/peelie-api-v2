ab01c0e6cacd65cf7c8f6bda0ef8d023da4892b8
heegu123 <kookminkhg@kookmin.ac.kr>
2026-04-26 21:42:34 +0900

    feat: 비동기 처리를 위한 가상 스레드 실행기 추가

--- src/main/java/kr/higu/peelie/common/config/AsyncConfig.java
+++ src/main/java/kr/higu/peelie/common/config/AsyncConfig.java
@@ -1,9 +1,17 @@
 package kr.higu.peelie.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig {
+
+    @Bean
+    public AsyncTaskExecutor virtualThreadTaskExecutor() {
+        return new VirtualThreadTaskExecutor("peelie-vt-");
+    }
 }
