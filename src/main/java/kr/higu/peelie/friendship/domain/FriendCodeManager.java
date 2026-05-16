3ede13e60318c018a23269b2683580d2cf862f10
heegu123 <kookminkhg@kookmin.ac.kr>
2026-05-16 15:00:44 +0900

    refactor: 친구 코드 정책 수정 -> 유효시간 기능 제거

--- src/main/java/kr/higu/peelie/friendship/domain/FriendCodeManager.java
+++
@@ -1,8 +0,0 @@
-package kr.higu.peelie.friendship.domain;
-
-import java.util.Optional;
-
-public interface FriendCodeManager {
-    FriendCode getOrCreate(Long userId);
-    Optional<Long> findUserIdByCode(String code);
-}
